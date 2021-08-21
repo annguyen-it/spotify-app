328581fb17369780f112069bd7fb3743f7fb4c88
annguyen <annguyen.it.utc@gmail.com>
2021-08-21 21:39:48 +0700

    Flatten directory /main-view

--- src/modules/main-view/main-view-routing.module.ts
+++ src/modules/main-view/main-view-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        loadChildren: async() => (await import('./home/home.module')).HomeModule
+        loadChildren: async() => (await import('../home/home.module')).HomeModule
       }
     ]
   }
