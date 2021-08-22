0cf373a46e1982e88056ccb1da25ab588dfcfe5d
annguyen <annguyen.it.utc@gmail.com>
2021-08-23 00:58:42 +0700

    Feat: Playlist-details

--- src/modules/home/home.module.ts
+++ src/modules/home/home.module.ts
@@ -2,17 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { PlaylistDetailsModule } from '@modules/shared/playlist-details/playlist-details.module';
 
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule,
+    PlaylistDetailsModule
   ],
-  declarations: [
-    HomeComponent
-  ],
-  exports: [
-    HomeComponent,
-  ]
+  declarations: [HomeComponent],
+  exports: [HomeComponent]
 })
 export class HomeModule { }
